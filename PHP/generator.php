4e3aa0dedded309abb62e7043cd21b93560b1631
Christian Stokkebekk <gfzilter@gmail.com>
2012-12-03 23:50:10 +0100

    Wrote it again in PHP

---
+++ PHP/generator.php
@@ -0,0 +1,66 @@
+<html>
+	<head>
+		This is a name Generator written by Christian Stokkebekk in Des. 2012.
+		The names generated are intended for fictional orginizations, such as guilds, shops and taverns.
+		The code should be available on my github: https://github.com/Zilter/guildNameGenerator
+	</head>
+	<body>
+		<form action="generator.php" method="get">
+			How many names do you want?
+			<br>
+			<input type="number" name="number"/>
+			<br>
+			<br>
+			<input type="submit" value="Generate" />
+		</form>
+	</body>
+</html>
+<?php
+
+function inputFile($filename) {
+	$file = file_get_contents($filename);
+	$readText = explode("\n", $file);
+
+	return $readText;
+}
+
+
+
+if (isset($_GET["number"])) {
+	
+	if ($_GET["number"] > 100) {
+		
+		$numberOfNames = 100;
+		echo "Requested too many names. Showing 100:\n\n";
+	} 
+	else {
+		$numberOfNames = $_GET["number"];
+	}
+	
+	$prefixChance = 40;
+	$adjectiveChance = 60;
+	$suffixChance = 30;
+
+	$prefix = inputFile("prefix.txt");
+	$adjective = inputFile("adjective.txt");
+	$noun = inputFile("noun.txt");
+	$suffix = inputFile("suffix.txt");
+
+	for ($i = 0; $i < $numberOfNames; $i++) {
+		if (rand(0, 99) < $prefixChance) {
+			echo $prefix[rand(0, sizeof($prefix) - 1)] . " ";
+		}
+
+		if (rand(0, 99) < $adjectiveChance) {
+			echo $adjective[rand(0, sizeof($adjective) - 1)] . " ";
+		}
+
+		echo $noun[rand(0, sizeof($noun) - 1)];
+
+		if (rand(0, 99) < $suffixChance) {
+			echo " " . $suffix[rand(0, sizeof($suffix) - 1)];
+		}
+		echo "<br>";
+	}
+}
+?>
